f17051ea36b3da9f4583d65fc8d29bd65ea76def
emibonezzi <emibonezzi@gmail.com>
2025-08-18 16:24:42 -0400

    Add aws lambda integration

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello from Go Lambda!",
+	}, nil
+}
 
 func main() {
-	fmt.Printf("Hello")
+	lambda.Start(handler)
 }
